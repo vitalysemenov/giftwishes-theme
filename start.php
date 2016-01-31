2dfb441377e473c4025f0b9d43aa6afffcd22b05
Vilaliy.Semenov <Vitaliy.Semenov@ottoruss.ru>
2016-01-31 21:58:22 +0300

    code

--- start.php
+++ start.php
@@ -1,6 +1,6 @@
 <?php defined('LARAVEL_START') or die('No direct script access allowed');
 
-set_meta('ADMINLTE::SKIN', 'skin-blue');
+set_meta('GIFTWISHES::SKIN', 'skin-blue');
 
 if (! App::bound('orchestra.avatar') && class_exists('Orchestra\Avatar\AvatarServiceProvider')) {
     App::register('Orchestra\Avatar\AvatarServiceProvider');
